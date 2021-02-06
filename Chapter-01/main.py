dfb968304fce740fd798024447059698c7b28e06
chm <1204295973@qq.com>
2021-02-06 13:02:49 +0800

    多线程

---
+++ Chapter-01/main.py
@@ -0,0 +1,18 @@
+# 多线程
+import threading
+import time
+
+
+def target(second):
+    print(f"Threading '{threading.current_thread().name}' is running.")
+    print(f"Threading '{threading.current_thread().name}' sleep {second}s.")
+    time.sleep(second)
+    print(f"Threading '{threading.current_thread().name}' ended.")
+
+
+print(f"Threading '{threading.current_thread().name}' is running.")
+for i in [1, 5]:
+    t = threading.Thread(target=target, args=[i])
+    t.start()
+    t.join()
+print(f"Threading '{threading.current_thread().name}' ended.")
